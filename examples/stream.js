ab4f800e3bcd32d95d1c269522e01fe3cc7ba006
Josh Bassett <josh.bassett@gmail.com>
2014-08-30 21:47:03 +1000

    Fix stream example.

--- examples/stream.js
+++ examples/stream.js
@@ -1,9 +1,9 @@
 var fkit    = require('../src/fkit'),
     fn      = fkit.fn,
     obj     = fkit.obj,
-    request = require('superagent'),
-    Bus     = fkit.Bus;
-    Stream  = fkit.Stream;
+    request = require('superagent');
+
+var Stream = fkit.Stream;
 
 var URL = 'http://www.random.org/strings/?num=@&len=8&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new';
 
@@ -20,14 +20,14 @@ function randomNumbers(count) {
 
   return Stream
     .fromCallback(f)
-    .map(obj.get('text'))
-    .map(splitOnNewline);
+    .map(fn.compose(splitOnNewline, obj.get('text')));
 }
 
-var up   = Stream.fromEvent(document.getElementById('up'), 'click').map(fn.constant(1)),
-    down = Stream.fromEvent(document.getElementById('down'), 'click').map(fn.constant(-1));
+var up   = Stream.fromEvent(document.getElementById('up'), 'click').map(fn.const(1)),
+    down = Stream.fromEvent(document.getElementById('down'), 'click').map(fn.const(-1));
 
-up.merge(down)
-  .scan(1, fn.add)
+up
+  .merge(down)
+  .scan(1, fn.compose(fn.max(1), fn.add))
   .flatMap(randomNumbers)
   .subscribe(fkit.log);
