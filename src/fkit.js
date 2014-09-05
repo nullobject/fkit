e823897c58b16d217f9ce6ca4c34da0c448e1c02
Josh Bassett <josh.bassett@gmail.com>
2014-09-05 20:47:06 +1000

    Alias the core module to fkit.

--- src/fkit.js
+++ src/fkit.js
@@ -2,10 +2,9 @@
 
 'use strict';
 
-module.exports = {
-  core:   require('./core'),
-  fn:     require('./fn'),
-  obj:    require('./obj'),
-  Bus:    require('./bus'),
-  Stream: require('./stream')
-};
+module.exports = require('./core');
+
+module.exports.fn     = require('./fn');
+module.exports.obj    = require('./obj');
+module.exports.Bus    = require('./bus');
+module.exports.Stream = require('./stream');
