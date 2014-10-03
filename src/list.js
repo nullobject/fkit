a9c6e142141306634aecedc3008bda4766ebb499
Josh Bassett <josh.bassett@gmail.com>
2014-10-03 22:23:29 +1000

    Documentation.

--- src/list.js
+++ src/list.js
@@ -6,10 +6,12 @@ var util = require('./util');
  * FKit treats both arrays and strings as *lists*: an array is a list of
  * elements, and a string is a list of characters.
  *
- * Representing strings as lists may be a novel concept for JavaScript users,
- * but it is quite common in other languages. This seemingly simple
- * abstractions yields a great deal of power. It allows you to use all the
- * functions and combinators you would normally use with arrays on strings.
+ * Representing strings as lists may be a novel concept for some JavaScript
+ * users, but it is quite common in other languages. This seemingly simple
+ * abstractions yields a great deal of power: it allows you to apply the same
+ * list combinators to both arrays and strings.
+ *
+ * @summary Working with Lists
  *
  * @module fkit/list
  * @mixes module:fkit/list/base
@@ -20,7 +22,6 @@ var util = require('./util');
  * @mixes module:fkit/list/set
  * @mixes module:fkit/list/sublist
  * @mixes module:fkit/list/zip
- * @summary Working with Lists
  * @author Josh Bassett
  */
 module.exports = util.extend({}, [
