336d12a53a09e7be4124b07cd0aa5ca736c97104
Josh Bassett <josh.bassett@gmail.com>
2018-12-31 13:05:03 +1100

    Extract unionBy function to file

--- src/list/set.js
+++ src/list/set.js
@@ -17,6 +17,7 @@ import pair from '../pair'
 import prepend from '../prepend'
 import pure from '../internal/pure'
 import tail from '../tail'
+import unionBy from '../unionBy'
 
 /**
  * This module defines set operations on lists.
@@ -24,37 +25,6 @@ import tail from '../tail'
  * @private
  * @module fkit/list/set
  */
-
-/**
- * Returns a list that contains the union of elements in the lists of `as` and
- * `bs`. The elements are compared using the comparator function `f`.
- *
- * The comparator function compares two elements, `a` and `b`. If the elements
- * are both considered to equal, then the comparator function should return
- * `true`. Otherwise it should return `false`.
- *
- * Duplicates are removed from `bs`, but if `as` contains duplicates then so
- * will the result.
- *
- * @summary Calculates the union of two lists.
- *
- * @example
- *
- * F.unionBy((a, b) => a === b, [1, 2, 3], [2, 3, 4]) // [1, 2, 3, 4]
- * F.unionBy((a, b) => a === b, 'hello', 'world') // 'hellowrd'
- *
- * @curried
- * @function
- * @param f A comparator function.
- * @param as A list.
- * @param bs A list.
- * @returns A new list.
- */
-export const unionBy = curry((f, as, bs) =>
-  fold((cs, b) => {
-    return any(a => f(a, b), as) ? cs : append(b, cs)
-  }, as, bs)
-)
 
 /**
  * Returns a list that contains the union of elements in the lists of `as` and
--- src/list/set.test.js
+++ src/list/set.test.js
@@ -1,34 +1,6 @@
 import * as set from '../../src/list/set'
 
 describe('list.set', () => {
-  describe('#unionBy', () => {
-    const f = (a, b) => a === b
-
-    it('handles an empty array', () => {
-      expect(set.unionBy(f)([1, 2, 3])([])).toEqual([1, 2, 3])
-      expect(set.unionBy(f)([])([1, 2, 3])).toEqual([1, 2, 3])
-    })
-
-    it('handles an empty string', () => {
-      expect(set.unionBy(f)('abc')('')).toBe('abc')
-      expect(set.unionBy(f)('')('abc')).toBe('abc')
-    })
-
-    it('handles an array', () => {
-      expect(set.unionBy(f)([1, 2, 3])([1, 2, 3])).toEqual([1, 2, 3])
-      expect(set.unionBy(f)([1, 2, 3])([2, 3, 4])).toEqual([1, 2, 3, 4])
-      expect(set.unionBy(f)([1, 2, 3])([4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6])
-      expect(set.unionBy(f)([1, 1])([1])).toEqual([1, 1])
-    })
-
-    it('handles a string', () => {
-      expect(set.unionBy(f)('abc')('abc')).toBe('abc')
-      expect(set.unionBy(f)('abc')('bcd')).toBe('abcd')
-      expect(set.unionBy(f)('abc')('def')).toBe('abcdef')
-      expect(set.unionBy(f)('aa')('a')).toBe('aa')
-    })
-  })
-
   describe('#union', () => {
     it('handles an empty array', () => {
       expect(set.union([1, 2, 3])([])).toEqual([1, 2, 3])
