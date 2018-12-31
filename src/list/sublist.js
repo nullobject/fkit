b9967a551836639fe9c4a4b34eecbfa0b236c9a6
Josh Bassett <josh.bassett@gmail.com>
2018-12-31 13:54:49 +1100

    Extract splitAt function to file

--- src/list/sublist.js
+++ src/list/sublist.js
@@ -20,25 +20,6 @@ import takeWhile from '../takeWhile'
 
 /**
  * Returns a list that contains the elements in the list of `as` split into a
- * pair of lists: a prefix of length `n` and the remainder of the list.
- *
- * @summary Splits a list.
- *
- * @example
- *
- * F.splitAt(1, [1, 2, 3]) // [[1], [2, 3]]
- * F.splitAt(1, 'foo') // ['f', 'oo']
- *
- * @curried
- * @function
- * @param n A number.
- * @param as A list.
- * @returns A pair of lists.
- */
-export const splitAt = curry((n, as) => [take(n, as), drop(n, as)])
-
-/**
- * Returns a list that contains the elements in the list of `as` split into a
  * pair of lists: a prefix of elements that satisfy the predicate function `p`
  * and the remainder of the list.
  *
--- src/list/sublist.test.js
+++ src/list/sublist.test.js
@@ -1,24 +1,6 @@
 import * as sublist from '../../src/list/sublist'
 
 describe('list.sublist', () => {
-  describe('#splitAt', () => {
-    it('handles an empty array', () => {
-      expect(sublist.splitAt(1)([])).toEqual([[], []])
-    })
-
-    it('handles an empty string', () => {
-      expect(sublist.splitAt(1)('')).toEqual(['', ''])
-    })
-
-    it('handles an array', () => {
-      expect(sublist.splitAt(1)([1, 2, 3])).toEqual([[1], [2, 3]])
-    })
-
-    it('handles a string', () => {
-      expect(sublist.splitAt(1)('foo')).toEqual(['f', 'oo'])
-    })
-  })
-
   describe('#span', () => {
     const p = a => a < 3
     const q = a => a !== 'o'
