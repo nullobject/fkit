6d270c73175b1c456407d664d3758c09af8e7e3e
Josh Bassett <josh.bassett@gmail.com>
2018-12-31 13:33:43 +1100

    Extract subsequences function to file

--- src/list/set.js
+++ src/list/set.js
@@ -26,35 +26,6 @@ import tail from '../tail'
  */
 
 /**
- * Returns a list that contains all the subsequences of the elements in the
- * list of `as`.
- *
- * @summary Calculates the subsequences of a list.
- *
- * @example
- *
- * F.subsequences([1, 2, 3]) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
- * F.subsequences('abc') // ['', 'a', 'b', 'ab', 'c', 'ac', 'bc', 'abc']
- *
- * @param as A list.
- * @returns A new list.
- */
-export function subsequences (as) {
-  const subsequences_ = bs => {
-    const b = head(bs)
-    const f = (ys, r) => concat(pure(ys), pure(prepend(b, ys)), r)
-
-    if (empty(bs)) {
-      return []
-    } else {
-      return prepend(pure(b), foldRight(f, [], subsequences_(tail(bs))))
-    }
-  }
-
-  return prepend(mempty(as), subsequences_(as))
-}
-
-/**
  * Returns a list that contains all the permutations of the elements in the
  * list of `as`.
  *
--- src/list/set.test.js
+++ src/list/set.test.js
@@ -1,24 +1,6 @@
 import * as set from '../../src/list/set'
 
 describe('list.set', () => {
-  describe('#subsequences', () => {
-    it('handles an empty array', () => {
-      expect(set.subsequences([])).toEqual([[]])
-    })
-
-    it('handles an empty string', () => {
-      expect(set.subsequences('')).toEqual([''])
-    })
-
-    it('handles an array', () => {
-      expect(set.subsequences([1, 2, 3])).toEqual([[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]])
-    })
-
-    it('handles a string', () => {
-      expect(set.subsequences('abc')).toEqual(['', 'a', 'b', 'ab', 'c', 'ac', 'bc', 'abc'])
-    })
-  })
-
   describe('#permutations', () => {
     it('handles an empty array', () => {
       expect(set.permutations([])).toEqual([[]])
