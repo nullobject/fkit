7951f21ab0a0fd1f0b0eb82eeaaade5c0ef60fc5
Josh Bassett <josh.bassett@gmail.com>
2019-02-09 13:50:12 +1100

    Include source maps

--- rollup.config.js
+++ rollup.config.js
@@ -13,8 +13,8 @@ export default [
   {
     input: 'src/index.js',
     output: [
-      { file: pkg.main, format: 'cjs', name: 'F' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
     ],
     plugins
   }, {
