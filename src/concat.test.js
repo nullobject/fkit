1e3aa705fd1b7663fc6f70189232738f1ee25251
Josh Bassett <josh.bassett@gmail.com>
2019-02-24 16:29:41 +1100

    Clean up concat test

--- src/concat.test.js
+++ src/concat.test.js
@@ -22,7 +22,6 @@ describe('#concat', () => {
   it('handles an array of numbers', () => {
     expect(concat([1, 2, 3, 4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6])
     expect(concat([[1], [2, 3], [4, 5, 6]])).toEqual([1, 2, 3, 4, 5, 6])
-
     expect(concat([[[1]], [[2, 3]], [[4, 5, 6]]])).toEqual([[1], [2, 3], [4, 5, 6]])
     expect(concat([[[1]], [[2, 3], [4, 5, 6]]])).toEqual([[1], [2, 3], [4, 5, 6]])
   })
@@ -30,7 +29,6 @@ describe('#concat', () => {
   it('handles an array of strings', () => {
     expect(concat(['foobar'])).toBe('foobar')
     expect(concat(['f', 'oo', 'bar'])).toBe('foobar')
-
     expect(concat([['f'], ['oo'], ['bar']])).toEqual(['f', 'oo', 'bar'])
     expect(concat([['f'], ['oo', 'bar']])).toEqual(['f', 'oo', 'bar'])
   })
@@ -40,7 +38,6 @@ describe('#concat', () => {
     expect(concat(1, 2, 3, ['foo'])).toEqual([1, 2, 3, 'foo'])
     expect(concat([1, 2, 3], 'foo')).toEqual([1, 2, 3, 'foo'])
     expect(concat([1, 2, 3], ['foo'])).toEqual([1, 2, 3, 'foo'])
-
     expect(concat('foo', 1, 2, 3)).toEqual(['foo', 1, 2, 3])
     expect(concat('foo', [1, 2, 3])).toEqual(['foo', 1, 2, 3])
     expect(concat(['foo'], 1, 2, 3)).toEqual(['foo', 1, 2, 3])
