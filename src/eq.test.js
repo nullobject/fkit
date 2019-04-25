3b0e835ccb6c7792b3a31dba0f70a44a76dccc87
Josh Bassett <hello@joshbassett.info>
2019-04-25 19:03:20 +1000

    Refactor eq test

--- src/eq.test.js
+++ src/eq.test.js
@@ -1,8 +1,11 @@
 import eq from './eq'
 
 describe('eq', () => {
-  it('returns true if the arguments are eq', () => {
-    expect(eq(1, 1)).toBe(true)
-    expect(eq(1, 2)).toBe(false)
+  it('returns true if the arguments are equal', () => {
+    expect(eq(1)(1)).toBe(true)
+  })
+
+  it('returns false otherwise', () => {
+    expect(eq(1)(2)).toBe(false)
   })
 })
